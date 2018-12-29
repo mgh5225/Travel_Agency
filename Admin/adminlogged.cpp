968fb844e334d6d835bb62e1d0b27d7d815fe32e
Alireza Nobakht <me@me.com>
2018-12-29 12:41:07 -0800

    1.remainder function added to accounts 2.admin signup fixed.

--- Admin/adminlogged.cpp
+++ Admin/adminlogged.cpp
@@ -1,23 +1,28 @@
 //
 // Created by Jarvis on 12/28/2018.
 //
-
+//header:
 #include "adminlogged.h"
-
+//necessary libraries:
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
 #include <windows.h>
-
+//necessary project files:
 #include "../main.h"
+#include "../BankAccounts/account_func.h"
+
 
 using namespace std;
 
 
 void adminLogged(){
     clrscr();
-    cout<<"hello world!";
+    int ac,pw;
+    cin>>ac>>pw;
+    cout<<remainder(ac,pw);
+
     int x;
     cin>>x;
 
